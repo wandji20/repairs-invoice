3e3b757312aad81fe6253829f175ef2afd2ac27a
wandji20 <wbertrand10@gmail.com>
2022-03-05 16:09:56 +0100

    Add invoice controller, update routes and generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :invoices, only: [:create, :index, :new]
   devise_for :users
+
+  root 'invoices#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
