1cbe57e7746a4dd362e5eb7ff9644f4c087e5872
wandji20 <wbertrand10@gmail.com>
2022-03-07 20:23:09 +0100

    Update invoice specs

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Invoice, type: :model do
   it { should validate_presence_of(:email) }
   it { should have_many(:invoice_items) }
+  it { should accept_nested_attributes_for(:invoice_items) }
 end
